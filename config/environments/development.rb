4f1443a7b59e00fd4b5ab27be9f177bc6f6ee80c
at-quando <quan.do@asiantech.vn>
2018-09-15 02:52:14 +0700

    adsf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.web_console.whitelisted_ips = '172.19.0.1'
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
